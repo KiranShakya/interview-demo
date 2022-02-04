d5357cde4993a65d37628266c8ff17f7646b3bfd
Amar Shilpakar <amarshilpakar@gmail.com>
2022-02-04 13:52:44 +0545

    bug fixes and added record count in table header

--- src/app/pipes/find-by-key-value/find-by-key-value.pipe.ts
+++ src/app/pipes/find-by-key-value/find-by-key-value.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FindByKeyValuePipe implements PipeTransform {
 
   transform(value: Array<{[key: string]: any}>, key: string, cmpValue: string): any {
-    return value.find(val => val[key] === cmpValue);
+      return value.find(val => val[key].split('@').shift() === cmpValue);
   }
 
 }
