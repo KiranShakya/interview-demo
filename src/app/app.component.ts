b32c3708846ee1adb89d43c9a6019a2241879670
Sagar Maharjan <sagarmaharjan31@gmail.com>
2022-08-11 18:22:43 +0545

    removed unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { BookService } from './data/book/service/book.service';
-import { Element } from './data/book/schema/element.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private readonly bookService: BookService) {}
+  constructor() {}
 
   ngOnInit() {}
 }
